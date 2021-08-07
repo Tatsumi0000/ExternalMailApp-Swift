96f29a65139d92efdd7ab2dc03e8941d8af92e8a
Tatsumi0000 <datsunote0211@gmail.com>
2021-08-07 20:19:36 +0900

    Second Commit

---
+++ Sources/ExternalMailApp-Swift/ExternalMailApp.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//
+//  Created by Tatsumi0000 on 2021/08/07.
+//
+
+struct ExternalMailApp {
+    var text = "Hello, World!"
+}
---
+++ Tests/ExternalMailApp-SwiftTests/ExternalMailApp_SwiftTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import ExternalMailApp_Swift
+
+    final class ExternalMailApp_SwiftTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(ExternalMailApp_Swift().text, "Hello, World!")
+        }
+    }
