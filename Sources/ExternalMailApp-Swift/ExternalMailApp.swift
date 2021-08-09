e6b5140b606b7724d4d2efe4b696b013698fa185
Tatsumi0000 <datsunote0211@gmail.com>
2021-08-09 23:55:23 +0900

    改行を置換するメソッドを追加

--- Sources/ExternalMailApp-Swift/ExternalMailApp.swift
+++ Sources/ExternalMailApp-Swift/ExternalMailApp.swift
@@ -4,6 +4,32 @@
 //  Created by Tatsumi0000 on 2021/08/07.
 //
 
+import Foundation
+
 struct ExternalMailApp {
     var text = "Hello, World!"
+    
+    let email: String
+    let subject: String
+    let body: String
+    
+    /// initializer
+    /// - Parameters:
+    ///   - email: To email
+    ///   - subject: Email title
+    ///   - body: Body
+    ///   - bodyReplaceRN: Which setting replace line breaks(/n with /r/n) in body text
+    init(email: String, subject: String, body: String, bodyReplaceRN: Bool = false) {
+        self.email = email
+        self.subject = subject
+        self.body = body
+    }
+    
+    /// Replace line breaks(/n with /r/n) in body text
+    /// - Parameter body: The string to be replaced
+    /// - Returns: Replaced String
+    func replaceBody(body: String) -> String {
+        return body.replacingOccurrences(of: "\n", with: "\r\n")
+    }
+
 }
